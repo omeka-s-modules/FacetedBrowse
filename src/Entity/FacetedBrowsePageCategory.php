2234d09659766f7140dc16330a70cf3cae245ce7
Jim Safley <jimsafley@gmail.com>
2021-04-20 20:56:48 -0400

    Unique constraint does not work well with collections

--- src/Entity/FacetedBrowsePageCategory.php
+++ src/Entity/FacetedBrowsePageCategory.php
@@ -7,13 +7,6 @@ use Omeka\Entity\AbstractEntity;
 
 /**
  * @Entity
- * @Table(
- *     uniqueConstraints={
- *         @UniqueConstraint(
- *             columns={"page_id", "category_id"}
- *         )
- *     }
- * )
  */
 class FacetedBrowsePageCategory extends AbstractEntity
 {
