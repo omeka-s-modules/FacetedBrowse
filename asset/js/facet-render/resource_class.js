e3f0ef0d030d00e1c1d4f787136ced368b71521f
Jim Safley <jimsafley@gmail.com>
2021-04-25 00:36:04 -0400

    Implement resource class facet

---
+++ asset/js/facet-render/resource_class.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+const container = $('#container');
+
+container.on('click', '.resource-class', function(e) {
+    const thisClass = $(this);
+    const facet = thisClass.closest('.facet');
+    const queries = [];
+    facet.find('.resource-class').not(thisClass).removeClass('selected');
+    thisClass.prop('checked', !thisClass.hasClass('selected'));
+    thisClass.toggleClass('selected');
+    facet.find('.resource-class.selected').each(function() {
+        const id = $(this).data('classId');
+        queries.push(`resource_class_id=${id}`);
+    });
+    facet.data('query', queries.join('&'));
+    container.trigger('faceted-browse:query-state-change');
+});
+
+});
