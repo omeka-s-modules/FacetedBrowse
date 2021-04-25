c3cc9ab98744b74d4023d2418100381ffeaa27d7
Jim Safley <jimsafley@gmail.com>
2021-04-24 23:51:33 -0400

    Render checkbox and radio for select types

--- asset/js/facet-render/property_literal.js
+++ asset/js/facet-render/property_literal.js
@@ -3,25 +3,16 @@ $(document).ready(function() {
 const container = $('#container');
 
 container.on('click', '.property-literal', function(e) {
-    e.preventDefault();
     const thisValue = $(this);
     const facet = thisValue.closest('.facet');
     const facets = container.find('.facet[data-facet-type="property_literal"]');
     const queries = [];
     let index = 0;
-    if ('multiple' === facet.data('facetData').select_type) {
-        thisValue.toggleClass('selected');
-    } else {
-        // Default select type is "single"
-        const notThisValue = facet.find('.property-literal').not(thisValue);
-        if (thisValue.hasClass('selected')) {
-            thisValue.removeClass('selected');
-            notThisValue.removeClass('disabled');
-        } else {
-            thisValue.addClass('selected');
-            notThisValue.addClass('disabled');
-        }
+    if ('single' === facet.data('facetData').select_type) {
+        facet.find('.property-literal').not(thisValue).removeClass('selected');
+        thisValue.prop('checked', !thisValue.hasClass('selected'));
     }
+    thisValue.toggleClass('selected');
     facets.each(function() {
         const thisFacet= $(this);
         const facetData = thisFacet.data('facetData');
